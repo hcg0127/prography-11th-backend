074eec16f7fe60d1adccb3cff39d6c4a19ec4c80
hcg0127 <kej431003@gmail.com>
2026-02-24 15:07:43 +0900

    feat: controller annotation 추가

--- src/main/java/com/prography/api/session/controller/SessionController.java
+++ src/main/java/com/prography/api/session/controller/SessionController.java
@@ -1,4 +1,14 @@
 package com.prography.api.session.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/")
 public class SessionController {
 }
